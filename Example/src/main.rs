12ccff7b90fbb4fd58e0ca545925debae9a7c9df
Alexander <alex.steve.elias@gmail.com>
2015-07-21 16:56:52 -0700

    Base Build

--- Example/src/main.rs
+++ Example/src/main.rs
@@ -4,13 +4,13 @@ fn main() {
 
     // Configure email body and header
     sendmail::email::create(
-        "noreply@verge.website",
-        "alex.steven.elias@gmail.com",
-        "This Is Subject",
-        "I am the body. hello wolrd!"
+        "noreply@example.com",
+        "aemail@gmail.com",
+        "This Is A Subject",
+        "I am the body. Hello Wolrd!"
     );
 
     // Define the actual email address to recieve the email
-    sendmail::email::send("alex.steven.elias@gmail.com");
+    sendmail::email::send("aemail@gmail.com");
 
 }
