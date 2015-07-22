af52ff21216fcf1835b9cd8f841d4fb5e65361df
Alexander <alex.steve.elias@gmail.com>
2015-07-22 09:24:45 -0700

    Fixed header not setting/Added html capability

--- Example/src/main.rs
+++ Example/src/main.rs
@@ -1,16 +1,20 @@
 extern crate sendmail;
+use sendmail::email;
 
 fn main() {
 
     // Configure email body and header
-    sendmail::email::create(
-        "noreply@example.com",
-        "aemail@gmail.com",
-        "This Is A Subject",
-        "I am the body. Hello Wolrd!"
+    email::create(
+        // From Address
+        "from.email@example.com",
+        // To Address
+        "to.email@example.com",
+        // Subject
+        "Subject - Hello World!",
+        // Body
+        "<html><body><h1>I am the body. Hello Wolrd!<br/><br/>And I accept html.</h1></body></html>"
     );
 
     // Define the actual email address to recieve the email
-    sendmail::email::send("aemail@gmail.com");
-
+    email::send("your.email@gmail.com");
 }
--- src/email/mod.rs
+++ src/email/mod.rs
@@ -5,21 +5,23 @@ use std::io::prelude::*;
 use std::process::Command;
 
 
-pub fn create (from_email_address:&str, to_email_address: &str, subject_text: &str, body_text: &str){
+pub fn create (from_address:&str, to_address: &str, subject_text: &str, body_text: &str){
 
-    let from_email_address = from_email_address.to_string();
-    let from = "\nFrom:".to_string() + &from_email_address;
+    let from_address = from_address.to_string();
+    let from = "From:".to_string() + &from_address + "\n";
 
-    let to_email_address = to_email_address.to_string();
-    let to = "\nTo:".to_string() + &to_email_address;
+    let to_address = to_address.to_string();
+    let to = "To:".to_string() + &to_address + "\n";
 
     let subject_text = subject_text.to_string();
-    let subject = "\nSubject:".to_string() + &subject_text;
+    let subject = "Subject:".to_string() + &subject_text + "\n";
 
-    let header = from + &to + &subject;
-    let body_text = body_text.to_string();
-    let body = "\n".to_string() + &body_text;
-    let header_plus_body = header + &body;
+    let content_type = "Content-Type: text/html".to_string() + "\n";
+    let header = from + &to + &subject + &content_type;
+
+    let body = body_text.to_string();
+
+    let header_body = header + &body;
 
     let path = Path::new("email.txt");
     let display = path.display();
@@ -31,18 +33,18 @@ pub fn create (from_email_address:&str, to_email_address: &str, subject_text: &s
     };
 
     // Write the 'text' string to 'file', returns 'io::Result<()>''
-    match file.write_all(header_plus_body.as_bytes()) {
+    match file.write_all(header_body.as_bytes()) {
         Err(why) => panic!("Couldn't write to {}: {}", display, Error::description(&why)),
         Ok(_) => println!("Successfully wrote to {}", display)
     }
 }
 
 
-pub fn send (to_email_address: &str) {
+pub fn send (to_address: &str) {
 
     // Send Email using sendmail -t
-    let to_email_address = to_email_address.to_string();
-    let sendmail_sh_frist_half = "sendmail ".to_string() + &to_email_address;
+    let to_address = to_address.to_string();
+    let sendmail_sh_frist_half = "sendmail ".to_string() + &to_address;
     let sendmail_sh_second_half = " < email.txt".to_string();
     let sendmail_sh = sendmail_sh_frist_half + & sendmail_sh_second_half;
 
