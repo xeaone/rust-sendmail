f0463869b55b3309d9c82e1b0c86ddad58eb98ef
Aron Heinecke <aron.heinecke@t-online.de>
2018-03-12 05:04:14 +0100

    allow Vec<&str> as address

--- src/email/mod.rs
+++ src/email/mod.rs
@@ -1,13 +1,17 @@
 use std::io::prelude::*;
 use std::process::{Command, Stdio};
 use std::io::{Error,ErrorKind};
-
 use std::io::BufReader;
+use std::convert::AsRef;
+
 const CONTENT_TYPE: &'static str = "Content-Type: text/html\n";
 
-pub fn send_new(from_address:&str, to_address: &[&str], subject_text: &str, body_text: &str) -> Result<(),Error> {
+pub fn send_new<'a ,T: AsRef<[&'a str]>>(from_address:&str, to_address: T, subject_text: &str, body_text: &str) -> Result<(),Error> {
+
+    let address: &[&str] = to_address.as_ref();
+
     let mut cmd = Command::new("sendmail");
-    cmd.args(to_address)
+    cmd.args(address)
         .stdin(Stdio::piped())
         .stdout(Stdio::null())
         .stderr(Stdio::piped());
@@ -17,7 +21,7 @@ pub fn send_new(from_address:&str, to_address: &[&str], subject_text: &str, body
         let mut stdin = process.stdin.as_mut().ok_or(Error::new(ErrorKind::BrokenPipe, "no stdin"))?;
         
         writeln!(&mut stdin,"From: {}",from_address)?;
-        writeln!(&mut stdin,"To: {}",to_address.join(","))?;
+        writeln!(&mut stdin,"To: {}",address.join(","))?;
         writeln!(&mut stdin,"Subject: {}",subject_text)?;
         stdin.write_all(CONTENT_TYPE.as_bytes())?;
         stdin.write_all(body_text.as_bytes())?;
@@ -28,7 +32,6 @@ pub fn send_new(from_address:&str, to_address: &[&str], subject_text: &str, body
     
     //let mut stdout_buffer = BufReader::new(process.stdout.take()?);
     let mut stderr_buffer = BufReader::new(process.stderr.ok_or(Error::new(ErrorKind::BrokenPipe, "no stderr"))?);
-    
     
     let mut stderr: String = String::new();
     stderr_buffer.read_to_string(&mut stderr)?;
