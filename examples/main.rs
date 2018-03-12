c53b924ff6d27fa96d4096c40d2330d40dc35ccf
Aron Heinecke <aron.heinecke@t-online.de>
2018-03-12 03:14:04 +0100

    cleanup repo, version up, readme update

--- examples/main.rs
+++ examples/main.rs
@@ -4,17 +4,14 @@ use sendmail::email;
 fn main() {
 
     // Configure email body and header
-    email::create(
+    email::send_new(
         // From Address
-        "from.email@example.com",
+        "test@localhost",
         // To Address
-        "to.email@example.com",
+        &["root@localhost"],
         // Subject
         "Subject - Hello World!",
         // Body
         "<html><body><h1>I am the body. Hello Wolrd!<br/><br/>And I accept html.</h1></body></html>"
-    );
-
-    // Define the actual email address to recieve the email
-    email::send("your.email@gmail.com");
+    ).unwrap();
 }
